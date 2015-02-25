e70849236d9006166f166864d108592426d9436b
ajakate <ajay.jakate@gmail.com>
2015-02-25 14:12:22 -0500

    it's more fun now

--- run.rb
+++ run.rb
@@ -1,22 +1,16 @@
-class Buddy
+module Buddy
 
   CYCLE = ["friend","buddy","pal","guy"]
 
-  def initialize
-    @buddy_index = [-1,0]
-  end
-
-  def print_next
-    @buddy_index.map! { |i| i == 3 ? 0 : i + 1 }
-    puts "I'm not your #{CYCLE[@buddy_index[0]]}, #{CYCLE[@buddy_index[1]]}"
+  def self.print
+    puts "I'm not your #{CYCLE[rand(0..3)]}, #{CYCLE[rand(0..3)]}"
     sleep 1
   end
 end
 
-buddy = Buddy.new
 loop do
   begin
-    buddy.print_next
+    Buddy.print
   rescue Exception => e
     puts "Hey! Stop it!"
   end
